fa0a09478b44070c757eede6a01f60d04505e50f
Lamp <ledlamp89@gmail.com>
2018-07-14 23:04:15 -0700

    idk how that got there

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Client = require("mpp-client");
 var client = new Client("ws://www.multiplayerpiano.com");
 client.setChannel("lobby");
 client.start();
-client.on("hi", ()=>{client.sendArray([
+client.on("hi", ()=>client.sendArray([
 	{m:'userset',set:{name:'𝒟𝑒𝓇𝓅'}},
 	{m:'a', message:'𝒟𝑒𝓇𝓅'}
 ]))
